e65fd9705245ed6757f6c17e4201f3b822b957d0
Khushikaur <gulatikhushi05@gmail.com>
2026-04-02 21:06:10 +0530

    Frontend api updated

--- apna-doctor-frontend/src/services/api.js
+++ apna-doctor-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://docnow-6ici.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
